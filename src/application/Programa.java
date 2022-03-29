0aa201d747be725fd7e443acb4bb0f1c8b65f931
Pamela Machado <pamela.wehle@gmail.com>
2022-03-29 01:51:40 -0300

    commit inicial

---
+++ src/application/Programa.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Vendedor;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Vendedor(a):");
+		String nome = sc.nextLine();
+		System.out.println("Meta: ");
+		double meta = sc.nextDouble();
+		System.out.println("Total Vendido: ");
+		double totalVendido = sc.nextDouble();
+	
+		Vendedor vendedor = new Vendedor(nome, meta, totalVendido);
+		
+		vendedor.calcularComissao();
+		 
+		System.out.println(vendedor);
+		
+		sc.close();
+		
+	}
+
+}
